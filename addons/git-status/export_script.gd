5e29c3db55ab310ccf529cb79ed178fc8e6f3653
Matt Giuca <matt.giuca@gmail.com>
2024-10-11 15:43:02 +1100

    Now also indicates whether any local modifications have been made.

--- addons/git-status/export_script.gd
+++ addons/git-status/export_script.gd
@@ -1,14 +1,18 @@
 @tool
 extends EditorExportPlugin
 
+const utility_script := preload('utility.gd')
+
 func _get_name() -> String:
   return 'GitStatus'
 
 func _export_begin(features: PackedStringArray, is_debug: bool, path: String, flags: int) -> void:
-  var git_hash : String
-  git_hash = preload('utility.gd').new().read_hash_from_git()
-  if git_hash.length() > 0:
-    print('Exporting with git hash: %s' % git_hash)
-    add_file('res://git-hash.txt', git_hash.to_ascii_buffer(), false)
+  var git_status : utility_script.Info
+  git_status = utility_script.new().read_status_from_git()
+  if git_status.hash.length() > 0:
+    print('Exporting with git hash: %s%s' %
+          [git_status.hash, '+changes' if git_status.modified else ''])
+    var json_text = git_status.to_json()
+    add_file('res://git-status.txt', json_text.to_ascii_buffer(), false)
   else:
     print('Exporting without git hash')
